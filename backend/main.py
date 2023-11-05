6dfb90f1c58654a04bc0c48dc156bbe4daa152e3
Paige Townsend <paigetownsend05@gmail.com>
2023-11-04 19:14:48 -0700

    added def

--- backend/main.py
+++ backend/main.py
@@ -5,11 +5,10 @@ This one file is all you need to start off with your FastAPI server!
 """
 
 from typing import Optional
-
 import uvicorn
 from fastapi import FastAPI, status
-
-
+import json 
+import requests
 
 # Initializing and setting configurations for your FastAPI application is one
 # of the first things you should do in your code.
@@ -35,34 +34,29 @@ def read_item(item_id: int, q: Optional[str] = None):
 if __name__ == "__main__":
     uvicorn.run("main:app", port=5000, reload=True)
 #Routes for Geoapify
-
-import requests
-#import geocoder
+KEY = '8141076d7f8142fd8fe25385b6d373da'
 
 user_location = input("Enter a City, Country: ")
-#address = (f'https://maps.googleapis.com/maps/api/geocode/json?{user_location}')
-#address.json
 
-import json 
-
-KEY = '8141076d7f8142fd8fe25385b6d373da'
 location = f"https://api.geoapify.com/v1/geocode/search?text={user_location}&format=json&apiKey={KEY}"
 location_info = requests.get(location)
 location_info = location_info.text
-
 data = json.loads(location_info)
-#print(data)
-limit = 15
+
 place_id = data['results']
 place_id = place_id[0]
 place_id = place_id['place_id']
+
 parameters = f"categories=tourism.attraction&filter=place:{place_id}&limit=15"
 
 url = (f"https://api.geoapify.com/v2/places?{parameters}&apiKey={KEY}")
 response = requests.get(url)
-def address_format(response):
+response = response.text
+places = json.loads(response)
+
+def address_format(list):
     collection = []
-    features = response['features']
+    features = list['features']
     for x in features:
         properties = x['properties']
         address = properties['formatted']
@@ -70,7 +64,22 @@ def address_format(response):
     collection.sort()
     return collection
 
+def get_long(list):
+    features = list['features']
+    for x in features:
+        properties = x['properties']
+        longitude = properties['lon'] 
+    return longitude
+
+def get_lat(list):
+    features = list['features']
+    for x in features:
+        properties = x['properties']
+        latitude = properties['lat'] 
+    return latitude
+
 list_of_attractions = address_format(places)
+
 
 #yelp for hotels
 
@@ -96,8 +105,21 @@ yelp_parameters = { 'latitude':f"{latitude}",
                     }
 yelp_url = 'https://api.yelp.com/v3/businesses/search'
 yelp_response= requests.get(yelp_url,params=yelp_parameters, headers=headers) 
-print(yelp_response)
-print(yelp_response.json())
-response = response.text
-places = json.loads(response)
+'''print(yelp_response)
+print(yelp_response.json())'''
 #print(places)
+
+
+for x in list_of_attractions:
+    tourist_yelp = []
+    lon = get_lon(x)
+    lat = get_lat(x)
+    tourist_parameters = { 'latitude':f"{lat}",
+                     'longitude':f"{lon}",
+                    'radius' : 40000,
+                    'limit' :5,
+                    }
+    yelp_url = 'https://api.yelp.com/v3/businesses/search'
+    tourist_response= requests.get(yelp_url,params=tourist_parameters, headers=headers)
+    tourist_yelp.append(tourist_response)
+print(tourist_yelp)
