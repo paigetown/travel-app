870e07d9b4f14e30d91ec570b958f4ec947ae5c9
Jonathan Pan <jonathanp2325@gmail.com>
2023-11-04 20:14:49 -0700

    new edition

--- backend/main.py
+++ backend/main.py
@@ -34,104 +34,104 @@ def read_item(item_id: int, q: Optional[str] = None):
 if __name__ == "__main__":
     uvicorn.run("main:app", port=5000, reload=True)
 #Routes for Geoapify
-KEY = '8141076d7f8142fd8fe25385b6d373da'
 
-user_location = input("Enter a City, Country: ")
+@app.get("/location/{location}")
+def get_location(location: str):
+    KEY = '8141076d7f8142fd8fe25385b6d373da'
 
-location = f"https://api.geoapify.com/v1/geocode/search?text={user_location}&format=json&apiKey={KEY}"
-location_info = requests.get(location)
-location_info = location_info.text
-data = json.loads(location_info)
+    location = f"https://api.geoapify.com/v1/geocode/search?text={location}&format=json&apiKey={KEY}"
+    location_info = requests.get(location)
+    location_info = location_info.text
+    data = json.loads(location_info)
 
-place_id = data['results']
-place_id = place_id[0]
-place_id = place_id['place_id']
+    place_id = data['results']
+    place_id = place_id[0]
+    place_id = place_id['place_id']
 
-parameters = f"categories=tourism.attraction&filter=place:{place_id}&limit=15"
+    parameters = f"categories=tourism.attraction&filter=place:{place_id}&limit=15"
 
-url = (f"https://api.geoapify.com/v2/places?{parameters}&apiKey={KEY}")
-response = requests.get(url)
-response = response.text
-places = json.loads(response)
+    url = (f"https://api.geoapify.com/v2/places?{parameters}&apiKey={KEY}")
+    response = requests.get(url)
+    return response.json()
 
-def address_format(list):
-    collection = []
-    features = list['features']
-    for x in features:
-        properties = x['properties']
-        address = properties['formatted']
-        collection.append(address)
-    collection.sort()
-    return collection
+@app.get("/attractions/{location}")
+def get_attractions(location: str):
 
-def get_lon(list):
-    features = list['features']
-    for x in features:
-        properties = x['properties']
-        longitude = properties['lon'] 
-    return longitude
+    def address_format(list):
+        collection = []
+        features = list['features']
+        for x in features:
+            properties = x['properties']
+            address = properties['formatted']
+            collection.append(address)
+        collection.sort()
+        return collection
 
-def get_lat(list):
-    features = list['features']
-    for x in features:
-        properties = x['properties']
-        latitude = properties['lat'] 
-    return latitude
+    def get_lon(list):
+        features = list['features']
+        for x in features:
+            properties = x['properties']
+            longitude = properties['lon'] 
+        return longitude
 
-list_of_attractions = address_format(places)
+    def get_lat(list):
+        features = list['features']
+        for x in features:
+            properties = x['properties']
+            latitude = properties['lat'] 
+        return latitude
 
-
-#yelp for hotels
-
-#getting longitude and latitude from previous geoapify call
-
-longitude = data['results']
-longitude = longitude[0]
-longitude = longitude['lon']
-
-latitude = data['results']
-latitude = latitude[0]
-latitude = latitude['lat']
+    data = get_location(location)
+    print(data)
+    list_of_attractions = address_format(data)
 
 
-yelp_key='rW67PZzaPE67n2ms9CobxWSC7dnglV2whOL5GK82WMNr1cP0U-g8uf_wPQnTJFcTaAGX60x-8zmM_oCzEDeoWZAT4v2SNl0guzmh906MicoEzrTYQTBMF6YKj8lGZXYx'
-headers = {'Authorization': 'Bearer %s' % yelp_key}
+    #yelp for hotels
 
-yelp_parameters = { 'latitude':f"{latitude}",
-                     'longitude':f"{longitude}",
-                    'term' : 'hotel', 
-                    'radius' : 40000,
-                    'limit' :6,
-                    }
-yelp_url = 'https://api.yelp.com/v3/businesses/search'
-yelp_response= requests.get(yelp_url,params=yelp_parameters, headers=headers)
-yelp_response = yelp_response.text
-hotels = json.loads(yelp_response)
+    #getting longitude and latitude from previous geoapify call
 
-##makes a dictionary for hotel names and images 
+    longitude = data['features'][0]['properties']['lon']
+    latitude = data['features'][0]['properties']['lat']
 
-def yelp_dictionary(dictionary):
-    yelp_collection = {}
-    businesses = dictionary.get('businesses', []) 
-    for x in businesses:
-        name = x.get('name', 'N/A')
-        image = x.get('image_url', 'N/A')
-        yelp_collection[name]=image
-    sorted_yelp_collection = dict(sorted(yelp_collection.items()))
-    return sorted_yelp_collection
 
-hotels_for_user= yelp_dictionary(hotels)
+    yelp_key='rW67PZzaPE67n2ms9CobxWSC7dnglV2whOL5GK82WMNr1cP0U-g8uf_wPQnTJFcTaAGX60x-8zmM_oCzEDeoWZAT4v2SNl0guzmh906MicoEzrTYQTBMF6YKj8lGZXYx'
+    headers = {'Authorization': 'Bearer %s' % yelp_key}
 
-for x in list_of_attractions:
-    tourist_yelp = []
-    lon = get_lon(x)
-    lat = get_lat(x)
-    tourist_parameters = { 'latitude':f"{lat}",
-                     'longitude':f"{lon}",
-                    'radius' : 40000,
-                    'limit' :5,
-                    }
+    yelp_parameters = { 'latitude':f"{latitude}",
+                        'longitude':f"{longitude}",
+                        'term' : 'hotel', 
+                        'radius' : 40000,
+                        'limit' :6,
+                        }
     yelp_url = 'https://api.yelp.com/v3/businesses/search'
-    tourist_response= requests.get(yelp_url,params=tourist_parameters, headers=headers)
-    tourist_yelp.append(tourist_response)
-    print(tourist_yelp)
+    yelp_response= requests.get(yelp_url,params=yelp_parameters, headers=headers)
+    yelp_response = yelp_response.text
+    hotels = json.loads(yelp_response)
+
+    ##makes a dictionary for hotel names and images 
+
+    def yelp_dictionary(dictionary):
+        yelp_collection = {}
+        businesses = dictionary.get('businesses', []) 
+        for x in businesses:
+            name = x.get('name', 'N/A')
+            image = x.get('image_url', 'N/A')
+            yelp_collection[name]=image
+        sorted_yelp_collection = dict(sorted(yelp_collection.items()))
+        return sorted_yelp_collection
+
+    return yelp_dictionary(hotels)
+
+    # for x in list_of_attractions:
+    #     tourist_yelp = []
+    #     lon = get_lon(x)
+    #     lat = get_lat(x)
+    #     tourist_parameters = { 'latitude':f"{lat}",
+    #                     'longitude':f"{lon}",
+    #                     'radius' : 40000,
+    #                     'limit' :5,
+    #                     }
+    #     yelp_url = 'https://api.yelp.com/v3/businesses/search'
+    #     tourist_response= requests.get(yelp_url,params=tourist_parameters, headers=headers)
+    #     tourist_yelp.append(tourist_response)
+    #     print(tourist_yelp)
